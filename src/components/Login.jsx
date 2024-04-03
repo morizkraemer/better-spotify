b25bc7225b2c345acb8b49c8755d911391958dec
Moriz Kraemer <morizkraemer@gmail.com>
2024-04-03 14:25:46 +0200

    Ready, Set, Go!

---
+++ src/components/Login.jsx
@@ -0,0 +1,9 @@
+export default function Login() {
+  return (
+    <div className="h-screen w-screen flex justify-center items-center">
+      <button className="w-56 h-16 rounded-full bg-sgreen text-black">
+        Login with Spotify
+      </button>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        sgreen: "#1bd954",
+      },
+    },
+  },
+  plugins: [],
+};
